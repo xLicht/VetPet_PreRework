130733802f45c65ddf570391670e1d749f43ea01
ChocorrolInCode <149702340+ChocorrolInCode@users.noreply.github.com>
2025-02-15 10:56:49 -0700

    Basura

---
+++ VetPet+/VeterinariaMenu.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace VetPet_
+{
+    partial class VeterinariaMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // VeterinariaMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Name = "VeterinariaMenu";
+            this.Text = "VeterinariaMenu";
+            this.Load += new System.EventHandler(this.VeterinariaMenu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
