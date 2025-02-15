f34dbec471759dbebeb6a7449c43210c2031b46f
wrok <brandonwrok@gmail.com>
2025-02-15 10:04:15 -0700

    fontawesome

--- VetPet+/AlmacenMenu.Designer.cs
+++ VetPet+/AlmacenMenu.Designer.cs
@@ -127,6 +127,7 @@
             this.button2.Text = "Pedidos";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
