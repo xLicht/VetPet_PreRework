ec1f7ca8018dd0b850915c6318aabb4f468599f3
ChocorrolInCode <149702340+ChocorrolInCode@users.noreply.github.com>
2025-02-16 12:45:50 -0700

    ModificarHistorialmedico

---
+++ VetPet+/VeterinariaModificarHistorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VetPet_
+{
+    public partial class VeterinariaModificarHistorial : Form
+    {
+        public VeterinariaModificarHistorial()
+        {
+            InitializeComponent();
+        }
+
+        private void VeterinariaModificarHistorial_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
