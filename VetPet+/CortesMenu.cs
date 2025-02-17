d8bea7c4959bd3a01e48796fb85b742801f65d3e
wrok <133279936+wrok8@users.noreply.github.com>
2025-02-17 10:39:28 -0700

    saludos

---
+++ VetPet+/CortesMenu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VetPet_
+{
+    public partial class CortesMenu : Form
+    {
+        private float originalWidth;
+        private float originalHeight;
+        private Dictionary<Control, (float width, float height, float left, float top, float fontSize)> controlInfo = new Dictionary<Control, (float width, float height, float left, float top, float fontSize)>();
+
+        private Form1 parentForm;
+        public CortesMenu()
+        {
+            InitializeComponent();
+            this.Load += CortesMenu_Load;       // Evento Load
+            this.Resize += CortesMenu_Resize;   // Evento Resize
+        }
+        public AlmacenMenu(Form1 parent)
+        {
+            InitializeComponent();
+            parentForm = parent;  // Guardamos la referencia del formulario principal
+        }
+
+        private void CortesMenu_Load(object sender, EventArgs e)
+        {
+            // Guardar el tamaño original del formulario
+            originalWidth = this.ClientSize.Width;
+            originalHeight = this.ClientSize.Height;
+
+            // Guardar información original de cada control
+            foreach (Control control in this.Controls)
+            {
+                controlInfo[control] = (control.Width, control.Height, control.Left, control.Top, control.Font.Size);
+            }
+        }
+
+        private void CortesMenu_Resize(object sender, EventArgs e)
+        {
+            // Calcular el factor de escala
+            float scaleX = this.ClientSize.Width / originalWidth;
+            float scaleY = this.ClientSize.Height / originalHeight;
+
+            foreach (Control control in this.Controls)
+            {
+                if (controlInfo.ContainsKey(control))
+                {
+                    var info = controlInfo[control];
+
+                    // Ajustar las dimensiones
+                    control.Width = (int)(info.width * scaleX);
+                    control.Height = (int)(info.height * scaleY);
+                    control.Left = (int)(info.left * scaleX);
+                    control.Top = (int)(info.top * scaleY);
+
+                    // Ajustar el tamaño de la fuente
+                    control.Font = new Font(control.Font.FontFamily, info.fontSize * Math.Min(scaleX, scaleY));
+                }
+            }
+        }
+    }
+}
