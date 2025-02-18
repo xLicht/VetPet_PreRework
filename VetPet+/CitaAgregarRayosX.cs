8a280b1f324d67f7834b1e7282f8cfaace8a6155
wrok <brandonwrok@gmail.com>
2025-02-17 20:15:15 -0700

    MIS MIL PANTALLAS QUITENME CHAMBA PERRAS

---
+++ VetPet+/CitaAgregarRayosX.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VetPet_
+{
+    public partial class CitaAgregarRayosX : Form
+    {
+        public CitaAgregarRayosX()
+        {
+            InitializeComponent();
+        }
+
+        private void CitaAgregarRayosX_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ VetPet+/CitaDueños.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace VetPet_
+{
+    partial class CitaDueños
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CitaDueños
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "CitaDueños";
+            this.Text = "CitaDueños";
+            this.Load += new System.EventHandler(this.CitaDueños_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
