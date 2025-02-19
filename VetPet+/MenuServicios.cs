72639befc2e4ea37cf0c5c5edf9c238bb06d6695
StarSer <androidemoc@gmail.com>
2025-02-19 13:24:59 -0700

    Borrado de Carpeta Servicios

--- VetPet+/MenuServicios.cs
+++ VetPet+/MenuServicios.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VetPet_
 {
-    public partial class AgregarTipoCirugia : Form
+    public partial class MenuServicios : Form
     {
-        public AgregarTipoCirugia()
+        public MenuServicios()
         {
             InitializeComponent();
         }
