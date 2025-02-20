0739b7433e4149d3f3678eb05e3bb77f903a31c4
wrok <133279936+wrok8@users.noreply.github.com>
2025-02-20 09:19:13 -0700

    alavergalamaster

--- VetPet+/AlmacenRecibirPedido.cs
+++ VetPet+/AlmacenRecibirPedido.cs
@@ -12,14 +12,37 @@ namespace VetPet_
 {
     public partial class AlmacenRecibirPedido : Form
     {
+        private float originalWidth;
+        private float originalHeight;
+        private Dictionary<Control, (float width, float height, float left, float top, float fontSize)> controlInfo = new Dictionary<Control, (float width, float height, float left, float top, float fontSize)>();
+
+        private Form1 parentForm;
+
         public AlmacenRecibirPedido()
         {
             InitializeComponent();
+            this.Load += AlmacenRecibirPedido_Load;       // Evento Load
+            this.Resize += AlmacenRecibirPedido_Resize;   // Evento Resize
         }
+
+        public AlmacenRecibirPedido(Form1 parent)
+        {
+            InitializeComponent();
+            parentForm = parent;  // Guardamos la referencia del formulario principal
+        }
+
 
         private void AlmacenRecibirPedido_Load(object sender, EventArgs e)
         {
+            // Guardar el tamaño original del formulario
+            originalWidth = this.ClientSize.Width;
+            originalHeight = this.ClientSize.Height;
 
+            // Guardar información original de cada control
+            foreach (Control control in this.Controls)
+            {
+                controlInfo[control] = (control.Width, control.Height, control.Left, control.Top, control.Font.Size);
+            }
         }
     }
 }
