081e78f19c4f796c8a27ef05cfe974f17f28c139
ChocorrolInCode <149702340+ChocorrolInCode@users.noreply.github.com>
2025-02-20 14:16:02 -0700

    Pantallas_en_Blanaco_Daniel

---
+++ VetPet+/DueConsultarDueño.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VetPet_
+{
+    public partial class DueConsultarDueño : Form
+    {
+        public DueConsultarDueño()
+        {
+            InitializeComponent();
+        }
+
+        private void DueConsultarDueño_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
