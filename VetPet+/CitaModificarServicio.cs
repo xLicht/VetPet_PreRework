e56cc1cfe30002e0a0f2bdc4a188bec110e826a6
wrok <brandonwrok@gmail.com>
2025-02-22 22:44:06 -0700

    sexo

--- VetPet+/CitaModificarServicio.cs
+++ VetPet+/CitaModificarServicio.cs
@@ -7,19 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VetPet_;
 
 namespace VetPet_
 {
-    public partial class CitaModificarServicio : Form
+    public partial class CitaModificarServicio : FormPadre
     {
         public CitaModificarServicio()
         {
             InitializeComponent();
         }
-
+        public CitaModificarServicio(Form1 parent)
+        {
+            InitializeComponent();
+            parentForm = parent;
+        }
         private void CitaModificarServicio_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            parentForm.formularioHijo(new CitaListaServicio(parentForm)); // Pasamos la referencia de Form1 a AlmacenInventarioAgregarProducto
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            parentForm.formularioHijo(new CitaListaServicio(parentForm)); // Pasamos la referencia de Form1 a AlmacenInventarioAgregarProducto
         }
     }
 }
