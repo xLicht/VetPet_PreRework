0541a8ddfaec4662d9cfbf52d77db2988da267df
ChocorrolInCode <149702340+ChocorrolInCode@users.noreply.github.com>
2025-02-22 19:13:16 -0700

    PantallasExtraDaniel

---
+++ VetPet+/DueListadoMascotas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VetPet_
+{
+    public partial class DueListadoMascotas : Form
+    {
+        public DueListadoMascotas()
+        {
+            InitializeComponent();
+        }
+
+        private void DueListadoMascotas_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
