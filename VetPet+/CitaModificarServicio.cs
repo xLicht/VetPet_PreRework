c48fe7adbb00a5d833a7217af9edb65ec266df17
wrok <brandonwrok@gmail.com>
2025-02-22 22:28:26 -0700

    A LA OTRA REPARTAN MEJOR EL PUTO TRABAJO :DDDDDDD

---
+++ VetPet+/CitaModificarServicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VetPet_
+{
+    public partial class CitaModificarServicio : Form
+    {
+        public CitaModificarServicio()
+        {
+            InitializeComponent();
+        }
+
+        private void CitaModificarServicio_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
