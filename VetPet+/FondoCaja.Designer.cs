d8b3b757f87d91f647337600fc38512547eb02e0
StarSer <androidemoc@gmail.com>
2025-02-25 18:54:20 -0700

    Actualizacion de Inicio sesion

--- VetPet+/FondoCaja.Designer.cs
+++ VetPet+/FondoCaja.Designer.cs
@@ -32,25 +32,27 @@
             this.BtnIngresar = new System.Windows.Forms.Button();
             this.TxtUsuario = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Salmon;
-            this.panel1.Controls.Add(this.BtnIngresar);
-            this.panel1.Controls.Add(this.TxtUsuario);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(316, 111);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(316, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(451, 354);
+            this.panel1.Size = new System.Drawing.Size(451, 553);
             this.panel1.TabIndex = 2;
             // 
             // BtnIngresar
             // 
             this.BtnIngresar.BackColor = System.Drawing.Color.LightCoral;
             this.BtnIngresar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnIngresar.Location = new System.Drawing.Point(130, 287);
+            this.BtnIngresar.Location = new System.Drawing.Point(85, 228);
             this.BtnIngresar.Name = "BtnIngresar";
             this.BtnIngresar.Size = new System.Drawing.Size(198, 46);
             this.BtnIngresar.TabIndex = 3;
@@ -61,7 +63,7 @@
             // TxtUsuario
             // 
             this.TxtUsuario.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtUsuario.Location = new System.Drawing.Point(70, 148);
+            this.TxtUsuario.Location = new System.Drawing.Point(32, 143);
             this.TxtUsuario.Name = "TxtUsuario";
             this.TxtUsuario.Size = new System.Drawing.Size(301, 29);
             this.TxtUsuario.TabIndex = 1;
@@ -70,11 +72,33 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(65, 99);
+            this.label1.Location = new System.Drawing.Point(27, 94);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(177, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "Fondo de Caja";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VetPet_.Properties.Resources.VetPet_Logo1;
+            this.pictureBox1.Location = new System.Drawing.Point(59, 286);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 188);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.BtnIngresar);
+            this.groupBox1.Controls.Add(this.TxtUsuario);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(45, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 511);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
             // 
             // FondoCaja
             // 
@@ -86,7 +110,9 @@
             this.Name = "FondoCaja";
             this.Text = "FondoCaja";
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -97,5 +123,7 @@
         private System.Windows.Forms.Button BtnIngresar;
         private System.Windows.Forms.TextBox TxtUsuario;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
