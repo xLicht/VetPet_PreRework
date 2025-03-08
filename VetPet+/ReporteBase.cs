091eef4b376fae67b9330bbf15295577d7816270
Carlos <celiscarlos654@gmail.com>
2025-03-08 01:12:18 -0700

    - Funcionamiento de generar pdf Ahora muestra el pdf de la razon de cita mas frecuente

---
+++ VetPet+/ReporteBase.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Pruebas_PDF
+{
+    public abstract class ReporteBase
+    {
+        protected Document Documento { get; private set; }
+        protected PdfWriter Writer { get; private set; }
+        protected string RutaPDF { get; private set; }
+        protected string DirectorioProyecto { get; private set; }
+
+        public ReporteBase(string nombreReporte, string idReporte)
+        {
+            DirectorioProyecto = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName;
+            string carpetaReportes = Path.Combine(DirectorioProyecto, "Reportes-Arch");
+
+            if (!Directory.Exists(carpetaReportes))
+            {
+                Directory.CreateDirectory(carpetaReportes);
+            }
+
+            // Genera el nombre dinámico del PDF
+            RutaPDF = Path.Combine(carpetaReportes, $"{nombreReporte}_{idReporte}.pdf");
+
+            Documento = new Document(PageSize.A4.Rotate());
+        }
+
+        public void GenerarReporte()
+        {
+            try
+            {
+                Writer = PdfWriter.GetInstance(Documento, new FileStream(RutaPDF, FileMode.Create));
+                Documento.Open();
+
+                AgregarEncabezado();
+                AgregarContenido();
+                AgregarPiePagina();
+
+                MessageBox.Show($"PDF generado correctamente en:\n{RutaPDF}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al generar PDF: " + ex.Message);
+            }
+            finally
+            {
+                Documento.Close();
+            }
+        }
+
+        protected virtual void AgregarEncabezado()
+        {
+            try
+            {
+                Font textoFont = FontFactory.GetFont(FontFactory.HELVETICA, 12);
+
+                string imagenPath = Path.Combine(DirectorioProyecto, "Resources", "VetPet_Logo1.png");
+                if (!File.Exists(imagenPath))
+                {
+                    MessageBox.Show("La imagen VetPetLogo.png no se encontró en la carpeta Resources.");
+                    return;
+                }
+
+                iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(imagenPath);
+                img.ScaleAbsolute(100f, 100f);
+                img.Alignment = Element.ALIGN_LEFT;
+
+                PdfPTable tablaEncabezado = new PdfPTable(2);
+                tablaEncabezado.WidthPercentage = 100;
+                tablaEncabezado.SetWidths(new float[] { 1, 3 });
+
+                PdfPCell celdaImagen = new PdfPCell(img);
+                celdaImagen.Border = PdfPCell.NO_BORDER;
+                celdaImagen.HorizontalAlignment = Element.ALIGN_LEFT;
+                tablaEncabezado.AddCell(celdaImagen);
+
+                PdfPCell celdaTextoEmpresa = new PdfPCell
+                (
+                    new Phrase("VetPet+ S.A de C.V\n\nLa Paz, Baja California Sur, México\n\nBlvd. Forjadores de Sudcalifornia\n\ncontacto.vetpetplus@vetpet.com\n\n+52 6121948332", textoFont)
+                );
+                celdaTextoEmpresa.Border = PdfPCell.NO_BORDER;
+                celdaTextoEmpresa.HorizontalAlignment = Element.ALIGN_RIGHT;
+                tablaEncabezado.AddCell(celdaTextoEmpresa);
+
+                Documento.Add(tablaEncabezado);
+                Documento.Add(new Paragraph("\n"));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en AgregarEncabezado: " + ex.Message);
+            }
+        }
+
+        protected abstract void AgregarContenido();
+
+        protected virtual void AgregarPiePagina()
+        {
+            Font textoFont = FontFactory.GetFont(FontFactory.HELVETICA, 12);
+
+            PdfPTable tablaFirmas = new PdfPTable(2);
+            tablaFirmas.WidthPercentage = 80;
+            tablaFirmas.SetWidths(new float[] { 1, 1 });
+
+            PdfPCell celdaFirma1 = new PdfPCell(new Phrase("\n\n\nFirma del Responsable\n\n\n\n________________________", textoFont));
+            PdfPCell celdaFirma2 = new PdfPCell(new Phrase("\n\n\nFirma del Administrador\n\n\n\n______________________", textoFont));
+
+            celdaFirma1.HorizontalAlignment = Element.ALIGN_CENTER;
+            celdaFirma2.HorizontalAlignment = Element.ALIGN_CENTER;
+            celdaFirma1.Border = PdfPCell.NO_BORDER;
+            celdaFirma2.Border = PdfPCell.NO_BORDER;
+
+            tablaFirmas.AddCell(celdaFirma1);
+            tablaFirmas.AddCell(celdaFirma2);
+
+            Documento.Add(tablaFirmas);
+        }
+    }
+}
--- VetPet+/ReportesCitas.Designer.cs
+++ VetPet+/ReportesCitas.Designer.cs
@@ -34,7 +34,7 @@
             this.pdfViewCita = new Patagames.Pdf.Net.Controls.WinForms.PdfViewer();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.BtnGenerar = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
@@ -46,10 +46,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Cascadia Mono", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(171)))), ((int)(((byte)(196)))));
-            this.label1.Location = new System.Drawing.Point(16, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(750, 106);
+            this.label1.Size = new System.Drawing.Size(607, 85);
             this.label1.TabIndex = 25;
             this.label1.Text = "Reportes: Citas";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -59,10 +58,9 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(91)))), ((int)(((byte)(131)))));
             this.button1.Font = new System.Drawing.Font("Cascadia Mono", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(171)))), ((int)(((byte)(196)))));
-            this.button1.Location = new System.Drawing.Point(53, 142);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(40, 115);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(349, 81);
+            this.button1.Size = new System.Drawing.Size(262, 66);
             this.button1.TabIndex = 26;
             this.button1.Text = "Razón de Cita más Frecuente";
             this.button1.UseVisualStyleBackColor = false;
@@ -72,10 +70,9 @@
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(91)))), ((int)(((byte)(131)))));
             this.button2.Font = new System.Drawing.Font("Cascadia Mono", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(171)))), ((int)(((byte)(196)))));
-            this.button2.Location = new System.Drawing.Point(53, 242);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(40, 197);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(349, 81);
+            this.button2.Size = new System.Drawing.Size(262, 66);
             this.button2.TabIndex = 27;
             this.button2.Text = "Razón de Cita menos Frecuente";
             this.button2.UseVisualStyleBackColor = false;
@@ -89,12 +86,12 @@
             this.pdfViewCita.FormHighlightColor = System.Drawing.Color.Transparent;
             this.pdfViewCita.FormsBlendMode = Patagames.Pdf.Enums.BlendTypes.FXDIB_BLEND_MULTIPLY;
             this.pdfViewCita.LoadingIconText = "Loading...";
-            this.pdfViewCita.Location = new System.Drawing.Point(588, 142);
-            this.pdfViewCita.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.pdfViewCita.Location = new System.Drawing.Point(441, 115);
+            this.pdfViewCita.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pdfViewCita.MouseMode = Patagames.Pdf.Net.Controls.WinForms.MouseModes.Default;
             this.pdfViewCita.Name = "pdfViewCita";
             this.pdfViewCita.OptimizedLoadThreshold = 1000;
-            this.pdfViewCita.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.pdfViewCita.Padding = new System.Windows.Forms.Padding(10, 10, 10, 10);
             this.pdfViewCita.PageAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.pdfViewCita.PageAutoDispose = true;
             this.pdfViewCita.PageBackColor = System.Drawing.Color.White;
@@ -105,7 +102,7 @@
             this.pdfViewCita.ShowCurrentPageHighlight = true;
             this.pdfViewCita.ShowLoadingIcon = true;
             this.pdfViewCita.ShowPageSeparator = true;
-            this.pdfViewCita.Size = new System.Drawing.Size(824, 469);
+            this.pdfViewCita.Size = new System.Drawing.Size(618, 381);
             this.pdfViewCita.SizeMode = Patagames.Pdf.Net.Controls.WinForms.SizeModes.FitToWidth;
             this.pdfViewCita.TabIndex = 36;
             this.pdfViewCita.TextSelectColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(244)))), ((int)(((byte)(171)))), ((int)(((byte)(196)))));
@@ -119,10 +116,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cascadia Mono", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(200)))), ((int)(((byte)(214)))));
-            this.label2.Location = new System.Drawing.Point(1204, 103);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(903, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(207, 35);
+            this.label2.Size = new System.Drawing.Size(156, 28);
             this.label2.TabIndex = 47;
             this.label2.Text = "Vista Previa";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -132,36 +128,34 @@
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(91)))), ((int)(((byte)(131)))));
             this.button3.Font = new System.Drawing.Font("Cascadia Mono", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(171)))), ((int)(((byte)(196)))));
-            this.button3.Location = new System.Drawing.Point(588, 620);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(441, 504);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(219, 55);
+            this.button3.Size = new System.Drawing.Size(164, 45);
             this.button3.TabIndex = 48;
             this.button3.Text = "Imprimir";
             this.button3.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // BtnGenerar
             // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(91)))), ((int)(((byte)(131)))));
-            this.button4.Font = new System.Drawing.Font("Cascadia Mono", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(171)))), ((int)(((byte)(196)))));
-            this.button4.Location = new System.Drawing.Point(1193, 620);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(219, 55);
-            this.button4.TabIndex = 49;
-            this.button4.Text = "Descargar";
-            this.button4.UseVisualStyleBackColor = false;
+            this.BtnGenerar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(91)))), ((int)(((byte)(131)))));
+            this.BtnGenerar.Font = new System.Drawing.Font("Cascadia Mono", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGenerar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(171)))), ((int)(((byte)(196)))));
+            this.BtnGenerar.Location = new System.Drawing.Point(895, 504);
+            this.BtnGenerar.Name = "BtnGenerar";
+            this.BtnGenerar.Size = new System.Drawing.Size(164, 45);
+            this.BtnGenerar.TabIndex = 49;
+            this.BtnGenerar.Text = "Generar";
+            this.BtnGenerar.UseVisualStyleBackColor = false;
+            this.BtnGenerar.Click += new System.EventHandler(this.BtnGenerar_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Cascadia Mono", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(200)))), ((int)(((byte)(214)))));
-            this.label3.Location = new System.Drawing.Point(963, 665);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(722, 540);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 35);
+            this.label3.Size = new System.Drawing.Size(72, 28);
             this.label3.TabIndex = 51;
             this.label3.Text = "Fecha";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -171,10 +165,9 @@
             this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(171)))), ((int)(((byte)(196)))));
             this.dateTimePicker1.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(815, 635);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Location = new System.Drawing.Point(611, 516);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(159, 26);
+            this.dateTimePicker1.Size = new System.Drawing.Size(120, 23);
             this.dateTimePicker1.TabIndex = 50;
             // 
             // dateTimePicker2
@@ -182,10 +175,9 @@
             this.dateTimePicker2.CalendarTitleBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(171)))), ((int)(((byte)(196)))));
             this.dateTimePicker2.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker2.Location = new System.Drawing.Point(1026, 635);
-            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker2.Location = new System.Drawing.Point(770, 516);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(159, 26);
+            this.dateTimePicker2.Size = new System.Drawing.Size(120, 23);
             this.dateTimePicker2.TabIndex = 52;
             // 
             // label4
@@ -193,32 +185,30 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Cascadia Mono", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(200)))), ((int)(((byte)(214)))));
-            this.label4.Location = new System.Drawing.Point(985, 628);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(739, 510);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 35);
+            this.label4.Size = new System.Drawing.Size(24, 28);
             this.label4.TabIndex = 60;
             this.label4.Text = "a";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // ReportesCitas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(9)))), ((int)(((byte)(48)))));
-            this.ClientSize = new System.Drawing.Size(1443, 710);
+            this.ClientSize = new System.Drawing.Size(1082, 577);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.BtnGenerar);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pdfViewCita);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ReportesCitas";
             this.Text = "ReportesCitas";
             this.ResumeLayout(false);
@@ -234,7 +224,7 @@
         private Patagames.Pdf.Net.Controls.WinForms.PdfViewer pdfViewCita;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button BtnGenerar;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
