96f81c478aed37c1d91426705a83cc37d359fb55
ChocorrolInCode <149702340+ChocorrolInCode@users.noreply.github.com>
2025-03-09 19:10:13 -0700

    Modificaciones en TipoEmpleado

--- VetPet+/EmpConsultarTipoEmpleado.cs
+++ VetPet+/EmpConsultarTipoEmpleado.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,23 +15,134 @@ namespace VetPet_
     public partial class EmpConsultarTipoEmpleado : FormPadre
     {
         public int DatoEmpleado { get; set; }
+        private conexionDaniel conexionDB = new conexionDaniel();
         public EmpConsultarTipoEmpleado(Form1 parent)
         {
             InitializeComponent();
             parentForm = parent;
-            cbInventario.AutoCheck = false;
+
             cbVentas.AutoCheck = false;
-            cbReceta.AutoCheck = false;
-            cbConsulta.AutoCheck = false;
-            cbAdministracion.AutoCheck = false;
             cbHistorialMedico.AutoCheck = false;
+            cbMascotas.AutoCheck = false;
+            cbDueños.AutoCheck = false;
+            cbCitas.AutoCheck = false;
+            cbCitasMedicas.AutoCheck = false;
+            cbMedicamentos.AutoCheck = false;
+            cbServicios.AutoCheck = false;
+            cbCortes.AutoCheck = false;
+            cbProvedores.AutoCheck = false;
+            cbPedidos.AutoCheck = false;    
+            cbProductos.AutoCheck = false;  
+            cbEmpleados.AutoCheck = false;  
+
         }
 
         private void EmpConsultarTipoEmpleado_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Dato Recibido:" + DatoEmpleado);
+            MessageBox.Show("Dato Recibido: " + DatoEmpleado);
+            CargarModulosTipoEmpleado();
         }
 
+
+        private void CargarModulosTipoEmpleado()
+        {
+            try
+            {
+                conexionDB.AbrirConexion();
+
+                string query = @"
+                    SELECT Modulo.nombre 
+                    FROM TipoEmpleado_Modulo
+                    INNER JOIN Modulo ON TipoEmpleado_Modulo.idModulo = Modulo.idModulo
+                    WHERE TipoEmpleado_Modulo.idTipoEmpleado = @idTipoEmpleado";
+
+                using (SqlCommand comandoSQL = new SqlCommand(query, conexionDB.GetConexion()))
+                {
+                    comandoSQL.Parameters.AddWithValue("@idTipoEmpleado", DatoEmpleado);
+                    SqlDataReader lectorSQL = comandoSQL.ExecuteReader();
+
+                    cbVentas.Checked = false;
+                    cbHistorialMedico.Checked = false;
+                    cbMascotas.Checked = false;
+                    cbDueños.Checked = false;
+                    cbCitas.Checked = false;
+                    cbCitasMedicas.Checked = false;
+                    cbMedicamentos.Checked = false;
+                    cbServicios.Checked = false;
+                    cbCortes.Checked = false;
+                    cbProvedores.Checked = false;
+                    cbPedidos.Checked = false;
+                    cbProductos.Checked = false;
+                    cbEmpleados.Checked = false;
+
+                    while (lectorSQL.Read())
+                    {
+                        string nombreModulo = lectorSQL["nombre"].ToString();
+
+                        if (nombreModulo == "Ventas")
+                        {
+                            cbVentas.Checked = true;
+                        }
+                        else if (nombreModulo == "Historiales Medicos")
+                        {
+                            cbHistorialMedico.Checked = true;
+                        }
+                        else if (nombreModulo == "Mascotas")
+                        {
+                            cbMascotas.Checked = true;
+                        }
+                        else if (nombreModulo == "Dueños")
+                        {
+                            cbDueños.Checked = true;
+                        }
+                        else if (nombreModulo == "Citas")
+                        {
+                            cbCitas.Checked = true;
+                        }
+                        else if (nombreModulo == "Citas Medicas")
+                        {
+                            cbCitasMedicas.Checked = true;
+                        }
+                        else if (nombreModulo == "Medicamentos")
+                        {
+                            cbMedicamentos.Checked = true;
+                        }
+                        else if (nombreModulo == "Servicios")
+                        {
+                            cbServicios.Checked = true;
+                        }
+                        else if (nombreModulo == "Cortes")
+                        {
+                            cbCortes.Checked = true;
+                        }
+                        else if (nombreModulo == "Proveedores")
+                        {
+                            cbProvedores.Checked = true;
+                        }
+                        else if (nombreModulo == "Pedidos")
+                        {
+                            cbPedidos.Checked = true;
+                        }
+                        else if (nombreModulo == "Productos")
+                        {
+                            cbProductos.Checked = true;
+                        }
+                        else if (nombreModulo == "Empleados")
+                        {
+                            cbEmpleados.Checked = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Error al obtener los módulos del tipo de empleado: " + error.Message);
+            }
+            finally
+            {
+                conexionDB.CerrarConexion();
+            }
+        }
         private void btnAceptar_Click(object sender, EventArgs e)
         {
             parentForm.formularioHijo(new EmpTiposEmpleados(parentForm));
