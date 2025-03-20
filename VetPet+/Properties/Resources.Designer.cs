d7e2519654632b911a4a1bf4ea11d3ee5874d90e
Carlos <celiscarlos654@gmail.com>
2025-03-19 21:04:34 -0700

    casi se va la master a la chingada cabros

--- VetPet+/Properties/Resources.Designer.cs
+++ VetPet+/Properties/Resources.Designer.cs
@@ -451,7 +451,7 @@ namespace VetPet_.Properties {
         }
         
         /// <summary>
-        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap icons8_add_button_25 {
             get {
